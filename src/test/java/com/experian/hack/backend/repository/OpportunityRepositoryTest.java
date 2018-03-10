b7576b926a18bfba457a9dd73c3f218d1204c56d
mboekhoff <Michiel.Boekhoff@experian.com>
2018-03-10 22:02:29 +0000

    Added embedded Neo4J instance to tests

--- src/test/java/com/experian/hack/backend/repository/OpportunityRepositoryTest.java
+++ src/test/java/com/experian/hack/backend/repository/OpportunityRepositoryTest.java
@@ -3,8 +3,12 @@ package com.experian.hack.backend.repository;
 import com.experian.hack.backend.node.Opportunity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.core.Is.is;
@@ -15,6 +19,19 @@ import static org.junit.Assert.assertThat;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class OpportunityRepositoryTest {
+
+    @TestConfiguration
+    public static class TestConfig {
+        @Bean
+        public SessionFactory getSessionFactory() {
+            return new SessionFactory( "com.experian.hack.backend");
+        }
+
+        @Bean
+        public Neo4jTransactionManager transactionManager() {
+            return new Neo4jTransactionManager(getSessionFactory());
+        }
+    }
 
     @Autowired
     private OpportunityRepository repo;
--- src/test/java/com/experian/hack/backend/repository/WorkerRepositoryTest.java
+++ src/test/java/com/experian/hack/backend/repository/WorkerRepositoryTest.java
@@ -2,10 +2,19 @@ package com.experian.hack.backend.repository;
 
 import com.experian.hack.backend.node.Worker;
 import org.hamcrest.collection.IsCollectionWithSize;
+import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
+import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -21,8 +30,26 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 public class WorkerRepositoryTest {
 
+    @TestConfiguration
+    public static class TestConfig {
+        @Bean
+        public SessionFactory getSessionFactory() {
+            return new SessionFactory( "com.experian.hack.backend");
+        }
+
+        @Bean
+        public Neo4jTransactionManager transactionManager() {
+            return new Neo4jTransactionManager(getSessionFactory());
+        }
+    }
+
     @Autowired
     private WorkerRepository repo;
+
+    @After
+    public void cleanUp() {
+        repo.deleteAll();
+    }
 
     @Test
     public void shouldSaveWorker() {
